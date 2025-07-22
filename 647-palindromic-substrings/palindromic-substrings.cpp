ef53a3b675dd0cd306a7eb2ca90f62c91cd1054b
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-07-23 02:48:55 +0530

    Time: 39 ms (25.24%) | Memory: 34.6 MB (15.33%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-   vector<vector<int>>dp;
+    vector<vector<int>> dp;
 
-   int check(string s,int i,int j){
-    if(i>=j)return 1;
-    if(dp[i][j]!=-1)return dp[i][j];
-
-    if(s[i]==s[j])return dp[i][j] =  check(s,i+1,j-1);
-    else return dp[i][j] =  0;
-   }
-
-
+    bool check(const string &s, int i, int j) {
+        if (i >= j) return true;
+        if (dp[i][j] != -1) return dp[i][j];
+        if (s[i] == s[j]) return dp[i][j] = check(s, i + 1, j - 1);
+        return dp[i][j] = false;
+    }
 
     int countSubstrings(string s) {
-        int count = 0;
-        int n = s.size();
-        dp.resize(n+1,vector<int>(n+1,-1));
-        for(int i=0;i<s.size();i++){
-            for(int j=i;j<s.size();j++){
-                     if(check(s,i,j))count++;
+        int n = s.length(), count = 0;
+        dp.resize(n, vector<int>(n, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (check(s, i, j)) count++;
             }
         }
         return count;

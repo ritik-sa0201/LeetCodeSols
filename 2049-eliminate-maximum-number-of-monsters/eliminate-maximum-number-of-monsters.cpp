1f974789167e2eb6cc07a27a42b00148393389a8
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-08-14 22:57:49 +0530

    Time: 31 ms (22.83%) | Memory: 87.4 MB (92.92%) - LeetSync

---
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        int n = dist.size();
+        vector<int> time(n);
+        
+        for (int i = 0; i < n; i++) {
+            time[i] = (dist[i] + speed[i] - 1) / speed[i]; 
+        }
+        
+        sort(time.begin(), time.end());
+        
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (time[i] <= i) break; 
+            ans++;
+        }
+        return ans;
+    }
+};

fab037213c169faf4e32fefdcfa9841b0eda1bf6
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-08-21 02:29:19 +0530

    Time: 4 ms (33.56%) | Memory: 14.2 MB (38.46%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+void solve(int i,int n,vector<int>&ans){
+    if(i>n)return;
+    ans.push_back(i);
+    for(int j=0;j<=9;j++){
+        int no = i*10+j;
+        solve(no,n,ans);
+        
+    }
+}
+public:
+    vector<int> lexicalOrder(int n) {
+         vector<int>ans;
+        for(int i=1;i<=9;i++){
+             solve(i,n,ans);
+        }
+         return ans;
+         
+    }
+};

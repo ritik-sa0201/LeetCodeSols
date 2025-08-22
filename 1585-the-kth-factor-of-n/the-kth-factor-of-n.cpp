ea51b67dceab1fc16c0ea5032d52b485d706d695
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-08-23 01:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (74.29%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        if(n==1 && k==1)return 1;
+        int count=0;
+     
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+                if(count==k){
+                    return i;
+                }
+            }
+        }
+        if(count<k)return -1;
+        return -1;
+    }
+};

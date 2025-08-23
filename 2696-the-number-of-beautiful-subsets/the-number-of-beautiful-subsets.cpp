fcbc489d0dbc56f3a40af17b6c8ffe9d68b69c7a
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-08-23 23:51:28 +0530

    Time: 306 ms (33.75%) | Memory: 37.5 MB (91.80%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int count=-1;
+
+   void solve(int i,vector<int>&nums,int k,vector<int>&op){
+    if(i==nums.size()){
+       for(int x=0;x<op.size();x++){
+        for(int j=x+1;j<op.size();j++){
+            if(abs(op[x]-op[j])==k)return;
+        }
+       }
+        count++;
+         return;
+    }
+       op.push_back(nums[i]);
+       solve(i+1,nums,k,op);
+       op.pop_back();
+       solve(i+1,nums,k,op);
+   }
+
+
+
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+         vector<int>op;
+         solve(0,nums,k,op);
+         return count;
+    }
+};

a0c08d9e956ebf2ce8afe350a6dca99340525728
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-08-27 20:07:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,12 +1,25 @@
+// class Solution {
+//     int solve(int n,vector<int>& dp){
+//         if(n==0 ||n==1 ||n==2) return n;
+//         if(dp[n]!=-1)return dp[n];
+//         return dp[n] = solve(n-1,dp)+solve(n-2,dp);
+//     } 
+// public:
+//     int climbStairs(int n) {
+//         vector<int>dp(n+1,-1);
+//          return solve(n,dp);
+//     }
+// };
 class Solution {
-    int solve(int n,vector<int>& dp){
-        if(n==0 ||n==1 ||n==2) return n;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
-    } 
 public:
     int climbStairs(int n) {
+        if(n==0 || n==1 || n==2)return n;
         vector<int>dp(n+1,-1);
-         return solve(n,dp);
+        dp[0]=0,dp[1]=1,dp[2]=2;
+        for(int i=3;i<=n;i++){
+           dp[i]=dp[i-1]+dp[i-2];
+        }
+
+         return dp[n];
     }
 };

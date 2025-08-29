f79da6ab5107f9cf90da4af47b6aa0456c04635c
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-08-29 14:33:22 +0530

    Time: 112 ms (38.09%) | Memory: 105.7 MB (33.09%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -73,7 +73,7 @@ public:
 
         dp.assign(n+1, vector<int>(n+1,0));
         for(int i=n-1;i>=0;i--){
-            for(int prev=n-1;prev>=-1;prev--){
+            for(int prev=i-1;prev>=-1;prev--){
                 int take = 0;
                 if (prev == -1 || isPred(words[prev], words[i])) {
                     take = 1 + dp[i+1][i+1];

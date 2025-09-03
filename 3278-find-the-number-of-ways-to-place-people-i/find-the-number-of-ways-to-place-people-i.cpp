0dab261fe77d0f17b84311faae8ef0a8a34c1f75
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-09-03 13:45:20 +0530

    Time: 0 ms (100.00%) | Memory: 32.4 MB (97.83%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -2,33 +2,39 @@ class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
-        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-            if (a[1] == b[1]) return a[0] < b[0];
-            return a[1] > b[1];
-        });
+        auto lambda = [](vector<int>& point1, vector<int>& point2) {
+            if (point1[0] == point2[0]) {
+                return point1[1] > point2[1];
+            }
+            return point1[0] < point2[0];
+        };
 
-        int count = 0;
+        sort(points.begin(), points.end(), lambda);
+
+        int result = 0;
 
         for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];  
+
+            int bestY = INT_MIN;
+
             for (int j = i + 1; j < n; j++) {
-      
-                if (points[i][0] <= points[j][0] && points[i][1] >= points[j][1]) {
-                    bool blocked = false;
+                int x2 = points[j][0];
+                int y2 = points[j][1];   
 
-                    for (int k = 0; k < n; k++) {
-                        if (k == i || k == j) continue;
+             
+                if (y2 > y1) { 
+                    continue;
+                }
 
-                        if (points[i][0] <= points[k][0] && points[k][0] <= points[j][0] &&
-                            points[j][1] <= points[k][1] && points[k][1] <= points[i][1]) {
-                            blocked = true;
-                            break;
-                        }
-                    }
-
-                    if (!blocked) count++;
+                if (y2 > bestY) {
+                    result++;
+                    bestY = y2;
                 }
             }
         }
-        return count;
+
+        return result;
     }
 };

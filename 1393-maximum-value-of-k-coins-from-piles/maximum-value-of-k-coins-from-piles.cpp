291d7bf7d47a5bc68b9f4ed173bc2318d70e8d8e
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-09-04 16:21:05 +0530

    Time: 142 ms (61.70%) | Memory: 34.2 MB (54.88%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
@@ -1,17 +1,19 @@
 class Solution {
     int m;
-    vector<vector<int>> prefix;
-    vector<vector<int>> dp;  
+    vector<vector<int>> dp;
 
-    int solve(int i, int k) {
+    int solve(int i, vector<vector<int>>& piles, int k) {
         if (k == 0) return 0;
+        if(k<0)return -1e9;
         if (i >= m) return 0;
         if (dp[i][k] != -1) return dp[i][k];
 
-        int ans = solve(i+1, k);  
-        int sz = prefix[i].size() - 1;
-        for (int x = 1; x <= min(sz, k); x++) {
-            ans = max(ans, prefix[i][x] + solve(i+1, k-x));
+        int ans = solve(i+1, piles, k); 
+        int len = piles[i].size();
+        int sum = 0;
+        for (int idx = 0; idx <len; idx++) {
+            sum += piles[i][idx];
+            ans = max(ans, sum + solve(i+1, piles, k - (idx+1)));
         }
 
         return dp[i][k] = ans;
@@ -20,16 +22,7 @@ class Solution {
 public:
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         m = piles.size();
-        prefix.resize(m);
-        for (int i = 0; i < m; i++) {
-            int sz = piles[i].size();
-            prefix[i].resize(sz+1, 0);
-            for (int j = 0; j < sz; j++) {
-                prefix[i][j+1] = prefix[i][j] + piles[i][j];
-            }
-        }
-
         dp.assign(m+1, vector<int>(k+1, -1));
-        return solve(0, k);
+        return solve(0, piles, k);
     }
 };

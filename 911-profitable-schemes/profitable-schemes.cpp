2d55b9a449deccd166762a9da205b87c1563fd86
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-09-04 19:16:53 +0530

    Time: 1185 ms (5.09%) | Memory: 677.6 MB (5.63%) - LeetSync

--- 911-profitable-schemes/profitable-schemes.cpp
+++ 911-profitable-schemes/profitable-schemes.cpp
@@ -1,36 +1,34 @@
 class Solution {
-    int maxn, mini;
+    int maxn,mini;
+    int mod = 1e9+7;
+    vector<vector<vector<int>>>dp;
+ 
     vector<int> gp;
-    vector<vector<vector<int>>> dp;
-
-    int solve(int i, int sum, int curr, vector<int>& profit) {
-        if (i == profit.size()) {
-            return (curr >= mini) ? 1 : 0;
+     int solve(int i,int sum,int curr,vector<int>&profit){
+        if(i==profit.size()){
+           if(curr>=mini) return 1;
+           return 0;
         }
-
-        if (dp[i][sum][curr] != -1) return dp[i][sum][curr];
-
-        long long count = 0;
-
-        if (sum + gp[i] <= maxn) {
-            int newProfit = min(mini, curr + profit[i]); 
-            count += solve(i + 1, sum + gp[i], newProfit, profit);
+        if(dp[i][sum][curr]!=-1)return dp[i][sum][curr];
+        int count = 0;
+        curr+=profit[i];
+        sum=sum+gp[i];
+        if(sum<=maxn){
+         count+=solve(i+1,sum,curr,profit);
         }
-
-        count += solve(i + 1, sum, curr, profit);
-
-        return dp[i][sum][curr] = count % 1000000007; 
-    }
+         curr-=profit[i];
+         sum=sum-gp[i];
+         count+=solve(i+1,sum,curr,profit);
+         return  dp[i][sum][curr] = count%mod;
+         }
 
 public:
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        maxn = n;
-        mini = minProfit;
-        gp = group;
-
-        dp.assign(profit.size() + 1,
-                  vector<vector<int>>(n + 1, vector<int>(minProfit + 1, -1)));
-
-        return solve(0, 0, 0, profit);
+        maxn=n,mini=minProfit;
+          dp.assign(profit.size() + 1,
+                  vector<vector<int>>(n + 1, vector<int>(8000, -1)));
+        gp=group;
+        vector<int>op;
+        return solve(0,0,0,profit);
     }
 };

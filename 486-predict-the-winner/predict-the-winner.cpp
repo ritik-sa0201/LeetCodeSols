cfa9b4caeadd9f64aa20309794e82b44e169f556
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-09-11 01:25:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (43.46%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,28 +1,18 @@
 class Solution {
-    vector<vector<vector<int>>> dp;
-
-    int score(int i, int j, int t, vector<int>& nums) {
+    int helper(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
         if (i > j) return 0;
-        if (dp[i][j][t] != -1) return dp[i][j][t];
+        if (dp[i][j] != -1) return dp[i][j];
 
-        if (t == 0) {  
-            int pl = nums[i] + score(i + 1, j, 1, nums);
-            int pr = nums[j] + score(i, j - 1, 1, nums);
-            return dp[i][j][t] = max(pl, pr);
-        } else { 
-            int pl = score(i + 1, j, 0, nums);
-            int pr = score(i, j - 1, 0, nums);
-            return dp[i][j][t] = min(pl, pr);
-        }
+        int left = nums[i] - helper(i + 1, j, nums, dp);
+        int right = nums[j] - helper(i, j - 1, nums, dp);
+
+        return dp[i][j] = max(left, right);
     }
 
 public:
     bool predictTheWinner(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
         int n = nums.size();
-        dp.assign(n, vector<vector<int>>(n, vector<int>(2, -1)));
-        int p1 = score(0, n - 1, 0, nums);
-        int p2 = total - p1;
-        return p1 >= p2;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return helper(0, n - 1, nums, dp) >= 0;
     }
 };

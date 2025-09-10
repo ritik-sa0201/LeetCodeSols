9e2e828c333684c16c4192442e4d7010098c0ee4
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-09-10 12:30:16 +0530

    Time: 213 ms (57.65%) | Memory: 153.2 MB (12.94%) - LeetSync

---
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
+            unordered_set<int> st;
+
+        for(auto &f : friendships) {
+            int u = f[0] - 1;
+            int v = f[1] - 1;
+
+            unordered_set<int> langSet(begin(languages[u]), end(languages[u]));
+            bool canTalk = false;
+            for(int lang : languages[v]) {
+                if(langSet.count(lang)) {
+                    canTalk = true;
+                    break;
+                }
+            }
+
+            if(!canTalk) {
+                st.insert(u);
+                st.insert(v);
+            }
+        }
+        vector<int> language(n+1, 0);
+        int maxi = 0;
+        for(int user : st) {
+            for(int lang : languages[user]) {
+                language[lang]++;
+                maxi = max(maxi, language[lang]);
+            }
+        }
+
+        return st.size() - maxi;
+           
+    }
+};

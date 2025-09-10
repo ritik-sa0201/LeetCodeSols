71e1f74eeaa086614d406b2829a309e1b4247feb
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-09-11 01:25:13 +0530

    Time: 3 ms (41.77%) | Memory: 11.3 MB (5.91%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,22 +1,27 @@
 class Solution {
+    vector<vector<vector<int>>> dp;
+
     int score(int i, int j, int t, vector<int>& nums) {
         if (i > j) return 0;
+        if (dp[i][j][t] != -1) return dp[i][j][t];
 
         if (t == 0) {  
             int pl = nums[i] + score(i + 1, j, 1, nums);
             int pr = nums[j] + score(i, j - 1, 1, nums);
-            return max(pl, pr);
+            return dp[i][j][t] = max(pl, pr);
         } else { 
             int pl = score(i + 1, j, 0, nums);
             int pr = score(i, j - 1, 0, nums);
-            return min(pl, pr);
+            return dp[i][j][t] = min(pl, pr);
         }
     }
 
 public:
     bool predictTheWinner(vector<int>& nums) {
         int total = accumulate(nums.begin(), nums.end(), 0);
-        int p1 = score(0, nums.size() - 1, 0, nums);
+        int n = nums.size();
+        dp.assign(n, vector<vector<int>>(n, vector<int>(2, -1)));
+        int p1 = score(0, n - 1, 0, nums);
         int p2 = total - p1;
         return p1 >= p2;
     }

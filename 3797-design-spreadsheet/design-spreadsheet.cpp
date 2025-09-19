2eefbd90250740b87103df5386e4ae62ae8b3b3e
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-09-20 03:12:53 +0530

    Time: 59 ms (85.83%) | Memory: 123 MB (89.30%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Spreadsheet {
+private:
+    unordered_map<string, int> cellValues;
+
+public:
+    Spreadsheet(int rows) {
+    }
+
+    void setCell(const string& cell, int value) {
+        cellValues[cell] = value;
+    }
+
+    void resetCell(const string& cell) {
+        cellValues[cell] = 0;
+    }
+
+    int getValue(string formula) {
+
+        formula = formula.substr(1);
+
+        size_t pos = formula.find('+');
+        string leftOperand = formula.substr(0, pos);
+        string rightOperand = formula.substr(pos + 1);
+
+        auto eval = [&](const string& op) {
+            if (!op.empty() && isdigit(op[0]))
+                return stoi(op);
+            return cellValues.count(op) ? cellValues[op] : 0;
+        };
+
+        return eval(leftOperand) + eval(rightOperand);
+    }
+};

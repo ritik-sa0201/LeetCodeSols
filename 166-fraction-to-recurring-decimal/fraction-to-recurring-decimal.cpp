d4f70355d948ba7505e363d4068fb8627f13bd9a
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-09-24 12:25:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (84.19%) - LeetSync

---
+++ 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) return "0";
+        
+        string result;
+        
+        if ((numerator < 0) ^ (denominator < 0))
+            result += "-";
+        
+        long long n = abs((long long)numerator);
+        long long d = abs((long long)denominator);
+
+        result += to_string(n / d);
+        long long remainder = n % d;
+        if (remainder == 0) return result;
+        
+        result += ".";
+        unordered_map<long long, int> seen;
+        while (remainder) {
+            if (seen.find(remainder) != seen.end()) {
+                result.insert(seen[remainder], "(");
+                result += ")";
+                break;
+            }
+            
+            seen[remainder] = result.size();
+            remainder *= 10;
+            result += to_string(remainder / d);
+            remainder %= d;
+        }
+        
+        return result;
+    }
+};
